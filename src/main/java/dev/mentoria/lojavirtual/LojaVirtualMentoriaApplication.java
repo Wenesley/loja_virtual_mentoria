2bbc9f1429392d398aeda14c3f341d8be41cd61e
Wenesley <wenesleysantos@gmail.com>
2023-01-10 20:02:53 -0300

    criado classe marcaproduto.

--- src/main/java/dev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/dev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package dev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
